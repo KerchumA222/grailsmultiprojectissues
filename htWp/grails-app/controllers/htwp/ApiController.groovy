6c5a125f3418a8cbbd42972a955dfac1de58dcf2
Alexander Justin Kerchum <KerchumA222@gmail.com>
2016-03-09 15:32:31 -0500

    Update ApiController.groovy
    Fix getting current user

--- htWp/grails-app/controllers/htwp/ApiController.groovy
+++ htWp/grails-app/controllers/htwp/ApiController.groovy
@@ -1,13 +1,17 @@
 package htwp
 
 import grails.converters.JSON
-import grails.plugin.springsecurity.annotation.Secured;
+import grails.plugin.springsecurity.annotation.Secured
+import grails.plugin.springsecurity.SpringSecurityService
+
 
 @Secured(['ROLE_ADMIN', 'ROLE_SERVER'])
 class ApiController {
+	
+    SpringSecurityService springSecurityService
 
     def index() { 
-		render getAuthenticatedUser() as JSON
-	}
+	render springSecurityService.currentUser as JSON
+    }
 }
 
